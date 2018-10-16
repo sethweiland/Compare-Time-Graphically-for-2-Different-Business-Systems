4084d6f3c88f9f14418ac2c9ed3071545b9252b5
Seth Weiland <42858933+sethweiland@users.noreply.github.com>
2018-10-15 22:30:06 -0700

    Update compare_time_and_graph.py

--- compare_time_and_graph.py
+++ compare_time_and_graph.py
@@ -3,15 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyodbc
 
-
+# lists of tuples that we will loop through with matching business lines used in 2 different systems
 queue_tuples= [ ('1018', 'Credit Review (CDL)'),('1017', 'Closing Review'),	('1020', 'DTI Review'),	('1016', 'Funding Review'), ('1008', 'Loan Structure Review (CDL)'), ('1021', 'Title Review')]
 encompass_tuples = [('1008', 'Loan Structure Review (CDL)'),('1016', 'Funding Review'),	('1017', 'Closing Review'),	('1018', 'Credit Review (CDL)'),('1020', 'DTI Review'),	('1021', 'Title Review')]
 
 
 
 queue_dict= dict(queue_tuples)
+
+#headers for one the pandas dataframes we will create
 encompass_headers=['process_name', 'month_start','avg_time_in_month(hours)', 'count_in_month']
 
+#two seperate pandas df, one for each business line studied
 df_allocation = pd.read_csv('count_average_per_month.csv')
 df_encompass = pd.read_csv('count_average_encompass.csv', header=None, names=encompass_headers)
 
@@ -21,12 +24,13 @@ print(len(unique_queue))
 for value, name in zip(queue_tuples, unique_encompass):
     if value[0] not in ('1008', '1016', '1017', '1018', '1020', '1021'):
         continue
-
+    #splice these dataframes within our loop to isolate all rows that represent a time transaction for a given business line
     df_specific_all = df_allocation[df_allocation['queue_id'] == int(value[0])]
     df_more_specific_all = df_specific_all[df_specific_all['created_month']<12]
     df_specific_encompass = df_encompass[df_encompass['process_name']==name]
     df_more_specific_encompass = df_specific_encompass[df_specific_encompass['month_start']<12]
-
+    #create plt figure and create 2 graphs that will sit in same figure, with each figure representing monthly averages times per transaction
+    #for given business periods
     fig=plt.figure(figsize = (10, 10))
     ax_all=fig.add_subplot(1,2,1)
     ax_enc = fig.add_subplot(1, 2, 2)
@@ -50,12 +54,14 @@ for value, name in zip(queue_tuples, unique_encompass):
     ax_enc.xaxis.set_major_locator(plt.MaxNLocator(12))
     bars=ax_all.patches
     bars1 = ax_enc.patches
+    #create histograms labels based on how many of each transaction happened in a given month to suplement the time-based analysis. 
     labels=[value for value in df_specific_all['count_fr_month']]
     for bar, label in zip(bars, labels):
         height=bar.get_height()
         ax_all.text(bar.get_x() + bar.get_width() /2, height+1,label, ha='center', va='bottom')
     file_id=str(queue_dict[str(value[0])])
-
-    file_name=('D:\\Users\\sweiland\\encompass_allocation_graphs\\' + file_id +'_('+str(value[0])+')'+'.png')
+    #file name edited out
+    file_name=('file name not given due to security concern' + file_id +'_('+str(value[0])+')'+'.png')
+    #save each pair of graphs to a png file within our loop for each business line. 
     plt.savefig(file_name, bbox_inches="tight")
     plt.show()
